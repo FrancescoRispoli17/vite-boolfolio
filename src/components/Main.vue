9120f2e32c3f190ba59f9ec76df70f84abef4e85
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-08-18 20:51:18 +0200

    [UPD] pagination

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,23 +8,36 @@
         },
         data() {
             return{
-                title:'Projects',
-                projects:[],            }
+                title: 'Projects',
+                projects:[],
+                postAPIpage: 1,            }
         },
         methods:{
             getProjects(){
                 const result = axios
-                .get('http://127.0.0.1:8000/api/project')
+                .get('http://127.0.0.1:8000/api/project', {
+                    params:{
+                        page:this.postAPIpage
+                    }
+                })
                 .then((response)=> {
                     console.log(response);
-                    if(response.data.status && response.data.results.length){
-                        console.log(response.data.results);
-                        this.projects = response.data.results;
+                    if(response.data.status && response.data.results.data.length){
+                        console.log(response.data.results.data);
+                        this.projects = response.data.results.data;
                     } else {
                         console.log('qualcosa è andato storto')
                     }
                 })
                 .catch((error) =>console.log(error));
+            },
+            changePage(page){
+                if(page < 1)
+                    page=1
+                else if(page>2)
+                    page=2;
+                this.postAPIpage=page;
+                this.getProjects();
             }
         },
         created(){
@@ -38,6 +51,14 @@
         <div class="row">
             <Project v-for="project in projects"
             :project="project" />
+            <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#" @click="changePage(postAPIpage-1)"><<</a></li>
+                <li class="page-item"><a class="page-link" href="#" @click="changePage(1)">1</a></li>
+                <li class="page-item"><a class="page-link" href="#" @click="changePage(2)">2</a></li>
+                <li class="page-item"><a class="page-link" href="#" @click="changePage(postAPIpage+1)">>></a></li>
+            </ul>
+            </nav>
         </div>
     </div>
 </template>
